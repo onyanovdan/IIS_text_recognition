23842ab4bd7fcf04a2f34cc4df335d4f5b287b10
vvaksman <waxvas@ya.ru>
2019-11-19 20:45:11 +0500

    Приборка
    - get_pict выделяет ключевые слова и ищет по ним картинку, возвращает ссылку на изображение

--- imgFind.py
+++ imgFind.py
@@ -4,19 +4,19 @@ import os
 from urllib.request import urlopen
 from urllib.request import Request
 from urllib.parse import urlencode
+from rutermextract import TermExtractor
 import os
-
 
 def get_soup(url, header):
 	data = urlencode(header).encode("utf-8")
 	r = requests.get(url,data=data)
 	return BeautifulSoup(r.content, from_encoding='utf-8',features=u'lxml')
 
-def getPict(descript):
+def find_pict(descript):
 	image_type = "img"
 	descript=descript.split()
-	#descript='+'.join(descript)
-	#url="https://yandex.ru/images/search?from=tabbar&text="+descript
+	#descript='+'.join(descript) яндекс
+	#url="https://yandex.ru/images/search?from=tabbar&text="+descript яндекс
 	descript='-'.join(descript)
 	url="https://ru.depositphotos.com/stock-photos/"+descript+".html"
 	header = {'User-Agent': 'Mozilla/5.0'}
@@ -26,13 +26,25 @@ def getPict(descript):
 	#image = soup.a.find_next("img", class_="serp-item__thumb")['src'] яндекс
 	image = soup.a.find_next("img", class_="file-container__image")['src']
 	#image='https:'+image яндекс
-	print(image)
-	raw_img = urlopen(image).read()
-	dir = r'.\images\\'
-	if not os.path.exists(dir): 
-		os.mkdir(dir)
-	cntr = len([i for i in os.listdir(dir) if image_type in i]) + 1
-	f = open(dir + image_type + "_"+ str(cntr)+".jpg", 'wb')
-	f.write(raw_img)
-	f.close()
+	#print(image)
+	#raw_img = urlopen(image).read()
+	#dir = r'.\images\\'
+	#if not os.path.exists(dir): 
+	#	os.mkdir(dir)
+	#cntr = len([i for i in os.listdir(dir) if image_type in i]) + 1	сохранение картинки в виде файла
+	#f = open(dir + image_type + "_"+ str(cntr)+".jpg", 'wb')
+	#f.write(raw_img)
+	#f.close()
 	return image
+	
+# возвращает или ссылку на картинку, или ничего	
+def get_pict(text):	
+	term_extractor = TermExtractor()
+	for term in term_extractor(text, nested=True):
+		print(term.normalized)
+		result = find_pict(term.normalized)
+		if result:
+			return result
+	return
+		
+#get_pict('Съешь ещё этих мягких французских булок да выпей же чаю.')
--- termextract.py
+++
@@ -1,8 +0,0 @@
-from imgFind import *
-from rutermextract import TermExtractor
-term_extractor = TermExtractor()
-text = u'Съешь ещё этих мягких французских булок да выпей же чаю.'
-for term in term_extractor(text, nested=True):
-	print(term.normalized)
-	if getPict(term.normalized):
-		break
