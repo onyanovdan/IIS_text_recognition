8662f385c2614917d3394374392a7b0a475c380e
Danila Onyanov <Onyanov.Danila@gmail.com>
2019-11-20 02:58:21 +0500

    вставка рисунка (информации) в дублированный документ

--- main.py
+++ main.py
@@ -32,18 +32,31 @@ def getTwoSubSplitsStr(paragr_split, indx):
 
 paragraphs = R.getParagraphsFromODT(fileName)
 morph = pymorphy2.MorphAnalyzer()
+result_buf = []
 for p_indx, paragr in enumerate(paragraphs):
+    images = []
     paragr_split = paragr.split()
     for w_indx, word in enumerate(paragr.split()):
         norm_word = morph.parse(word)[norm_word_order].normal_form
         # print(norm_word)
         if norm_word in triggered_words:
             # print('found - ' + norm_word)
+            _image = []
             key_words_arr = getTwoSubSplitsStr(paragr_split, w_indx)
             print(key_words_arr)
             img = None
+            first = True
             if key_words_arr[0] != '':
               img, term = I.get_pict(key_words_arr[0])
             if img == None and key_words_arr[1] != '':
                 img, term = I.get_pict(key_words_arr[1])
+                first = False
             print(img, term)
+            if img:
+                to_insert = w_indx + 1 if not first else w_indx + key_words_count + 1
+                _image.append(to_insert)
+                _image.append(term)
+                _image.append(img)
+                images.append(_image)
+    result_buf.append(images)
+R.writeUpdatedODT(fileName, result_buf)
--- read_odt.py
+++ read_odt.py
@@ -1,3 +1,5 @@
+import os
+
 from bs4 import BeautifulSoup
 #from odf import text, teletype
 
@@ -15,3 +17,44 @@ def getParagraphsFromODT(filename):
     for xml in xmls:
         texts.append(xml.text)
     return texts
+
+def insertImage(text, word_place, value):
+    text_split = text.split()
+    text_split.insert(word_place, value)
+    return " ".join(text_split)
+def writeUpdatedODT(fileName, images_buf):
+    # os.system('copy ' + fileName + ' updated_' + fileName)
+    up_fileName = 'updated_' + fileName
+    try:
+        os.remove(up_fileName)
+    except:
+        None
+    myFile = zipfile.ZipFile(fileName)
+    listOfFiles = myFile.infolist()
+    texts = []
+    with zipfile.ZipFile(up_fileName, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
+        for s in listOfFiles:
+            zf.comment = myFile.comment
+            if s.orig_filename == 'content.xml':
+                content = myFile.read(s.orig_filename).decode("utf-8")
+                doc = BeautifulSoup(content, features=u"xml")
+                xmls = doc.findAll('text:p')
+                print(doc.get('text:p'))
+            else:
+                content = myFile.read(s.filename)
+                zf.writestr(s, content)
+
+    count_images = 0
+    for x_indx, xml in enumerate(xmls):
+        _str = xml.text
+        for i_indx, image in reversed(list(enumerate(images_buf[x_indx]))):
+            _str = insertImage(_str, image[0], 'Рис. ' + str(count_images + i_indx + 1) + ' ' + image[1] + ' (' + image[2] + ')')
+        count_images += len(images_buf[x_indx])
+        xml.string = _str
+    # print(doc.find('text:p'))
+    with zipfile.ZipFile(up_fileName, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
+        zf.writestr('content.xml', doc.encode('utf-8'))
+
+fileName = 'текст.odt'
+buf = [[[2,'test','test.img']],[],[],[],[]]
+writeUpdatedODT(fileName, buf)
