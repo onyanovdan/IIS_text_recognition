6e7f6d7e4f38c79e3c32d9856080917925f172b5
Danila Onyanov <Onyanov.Danila@gmail.com>
2019-11-20 00:38:30 +0500

    Получение ссылки без вывода в файл

---
+++ main.py
@@ -0,0 +1,49 @@
+import read_odt as R
+import pymorphy2
+import imgFind as I
+
+norm_word_order = 0 #может сломаться, если индекс выйдет за диапазон
+fileName = 'текст.odt'
+triggered_words = ['проиллюстрировать'
+                   ,'показать'
+                   ,'пример'
+                   ,'увидеть'
+                   ,'рисунок'
+                    ]
+key_words_count = 4
+right_key_words_first = True
+
+def splitToString(word_arr):
+    str = " "
+    return str.join(word_arr)
+def getTwoSubSplitsStr(paragr_split, indx):
+    word_arrs = []
+    right = splitToString(paragr_split[indx+1:indx+1+key_words_count])
+    word_arrs.append(right)
+    min_indx = indx-key_words_count
+    min_indx = 0 if min_indx < 0 else min_indx
+    left = splitToString(paragr_split[min_indx:indx])
+    if right_key_words_first:
+        word_arrs.append(left)
+    else:
+        word_arrs.insert(0,left)
+    return word_arrs
+
+
+paragraphs = R.getParagraphsFromODT(fileName)
+morph = pymorphy2.MorphAnalyzer()
+for p_indx, paragr in enumerate(paragraphs):
+    paragr_split = paragr.split()
+    for w_indx, word in enumerate(paragr.split()):
+        norm_word = morph.parse(word)[norm_word_order].normal_form
+        # print(norm_word)
+        if norm_word in triggered_words:
+            # print('found - ' + norm_word)
+            key_words_arr = getTwoSubSplitsStr(paragr_split, w_indx)
+            print(key_words_arr)
+            img = None
+            if key_words_arr[0] != '':
+              img = I.get_pict(key_words_arr[0])
+            if img == None and key_words_arr[1] != '':
+                img = I.get_pict(key_words_arr[1])
+            print(img)
--- read_odt.py
+++ read_odt.py
@@ -3,17 +3,15 @@ from bs4 import BeautifulSoup
 
 import sys, zipfile, io
 
-fileName = 'текст.odt'
-content = ''
-myFile  = zipfile.ZipFile(fileName)
-listOfFiles = myFile.infolist()
-for s in listOfFiles:
-    # print(s.orig_filename)
-    # content = myFile.read(s.orig_filename)
-    # print(content)
-    if s.orig_filename == 'content.xml':
-        content = myFile.read(s.orig_filename).decode("utf-8")
-        # text = io.TextIOWrapper(io.BytesIO(content.read()))
-        doc = BeautifulSoup(content, features="html.parser")
-        text = doc.findAll('text:p')
-        print(text)
+def getParagraphsFromODT(filename):
+    myFile  = zipfile.ZipFile(filename)
+    listOfFiles = myFile.infolist()
+    texts = []
+    for s in listOfFiles:
+        if s.orig_filename == 'content.xml':
+            content = myFile.read(s.orig_filename).decode("utf-8")
+            doc = BeautifulSoup(content, features=u"lxml")
+            xmls = doc.findAll('text:p')
+    for xml in xmls:
+        texts.append(xml.text)
+    return texts
